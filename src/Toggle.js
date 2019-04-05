f8f42ee29a433d1e8aff2a680aab2e563ac15dcf
Rachel Ruderman <rachel@yembo.ai>
2019-04-04 20:02:24 -0700

    Refactor Toggle

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,21 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class Refactor extends Component {
-    state = {
-        isToggled: false
-    };
+const Toggle = () => {
 
-    toggle = () => {
-        this.setState(state => {
-            return { isToggled: !state.isToggled };
-        });
-    };
+    const [isToggled, setToggle] = useState(false);
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.toggle}>Toggle</button>
-                {this.state.isToggled && <h2>Hello!</h2>}
-            </div>
-        );
-    }
+    return (
+        <div>
+            <button onClick={() => setToggle(!isToggled)}>
+                Toggle Me
+            </button>
+            {isToggled && <h2>Hello</h2>}
+        </div>
+    )
+};
+
+export default Toggle;
+//
+// export default class Refactor extends Component {
+//     state = {
+//         isToggled: false
+//     };
+//
+//     toggle = () => {
+//         this.setState(state => {
+//             return { isToggled: !state.isToggled };
+//         });
+//     };
+//
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.toggle}>Toggle</button>
+//                 {this.state.isToggled && <h2>Hello!</h2>}
+//             </div>
+//         );
+//     }
