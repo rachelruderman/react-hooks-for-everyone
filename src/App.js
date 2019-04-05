cf92769fc7bf9445d7749e6043fe5a99974ca17b
Rachel Ruderman <rachel@yembo.ai>
2019-04-04 20:43:37 -0700

    Import hook from another folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {useTitleInput} from "./hooks/index";
 
 const App = () => {
-
-    const [name, setName] = useState('');
+    const [name, setName] = useTitleInput('');
 
     return (
         <div className="main-wrapper">
-          <h1>Level Up Dishes</h1>
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+            <h1>Level Up Dishes</h1>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+            }}>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+            </form>
         </div>
     );
 };
---
+++ src/hooks/index.js
@@ -0,0 +1,11 @@
+import {useState, useEffect} from 'react';
+
+export const useTitleInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+
+    useEffect(() => {
+        document.title = value
+    });
+
+    return [value, setValue];
+};
