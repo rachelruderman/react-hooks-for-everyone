e4a114f5e251a7e50a7ff84e4e2938ae18b1e5b1
Rachel Ruderman <rachel@yembo.ai>
2019-04-16 22:07:27 -0700

    Example: useMemo hook (aka shouldFunctionRun)

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, createContext} from 'react';
+import React, {useRef, createContext, useMemo} from 'react';
 import {useTitleInput} from "../hooks/index";
 import Toggle from './Toggle';
 import Counter from './Counter';
@@ -7,13 +7,17 @@ export const UserContext = createContext();
 
 const App = () => {
     const [name, setName] = useTitleInput('');
-    const ref       = useRef();
-    const onClick   = () => ref.current.classList.add('new-fake-class');
+    const ref           = useRef();
+    const onClick       = () => ref.current.classList.add('new-fake-class');
+
+    const reverseWord   = (word) => word.split('').reverse().join('');
+    const title         = 'Level Up Dishes';
+    const titleReversed = useMemo(() => reverseWord(title), [title]);
 
     return (
         <UserContext.Provider value={{user: true}}>
             <div className="main-wrapper" ref={ref}>
-                <h1 onClick={onClick}>Level Up Dishes</h1>
+                <h1 onClick={onClick}>{titleReversed}</h1>
                 <Toggle/>
                 <Counter/>
                 <form onSubmit={(e) => e.preventDefault()}>
