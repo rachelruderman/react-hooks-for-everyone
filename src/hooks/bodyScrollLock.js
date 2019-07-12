c8f8a895322b5f009ed82ef330d4165688b7e06a
Rachel Ruderman <rachel@yembo.ai>
2019-07-12 14:27:48 -0700

    How to clean up hooks (similar to componentWillUnmount)

--- src/hooks/bodyScrollLock.js
+++ src/hooks/bodyScrollLock.js
@@ -5,5 +5,10 @@ export const useBodyScrollLock = () => {
     // Good for waiting for width/height of element
     useLayoutEffect(() => {
         document.body.style.overflow = 'hidden';
+
+        // When the effects of the hook go away, the return function is run
+        return () => {
+            document.body.style.overflow = '';
+        }
     })
 };
