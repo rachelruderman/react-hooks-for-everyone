0b6b057006ac15d93047765e1d707dafcb0867a6
Rachel Ruderman <rachel@yembo.ai>
2019-07-12 14:29:00 -0700

    Clean up

--- src/hooks/bodyScrollLock.js
+++ src/hooks/bodyScrollLock.js
@@ -4,11 +4,12 @@ export const useBodyScrollLock = () => {
     // Waits for the DOM to be fully rendered
     // Good for waiting for width/height of element
     useLayoutEffect(() => {
+        const originalOverflow       = window.getComputedStyle(document.body).overflow;
         document.body.style.overflow = 'hidden';
 
         // When the effects of the hook go away, the return function is run
         return () => {
-            document.body.style.overflow = '';
+            document.body.style.overflow = originalOverflow;
         }
-    })
+    }, []) //Add empty array to make sure this gets run only once; not every time the component updates
 };
